386e33b8462b8700fec42e553d518b0c00b59547
rsziller <48862921+rsziller@users.noreply.github.com>
2021-11-30 11:49:59 -0600

    Carga de documentos fase 2
    Carga de archivos con gramaticas de prueba en la carpeta archivos de prueba y

---
+++ README.md
@@ -0,0 +1,17 @@
+# Compiladores Proyecto
+Proyecto Compiladores 2021
+
+# Integrantes
+
+- Rodrigo Ramírez 1156813
+- Rolando Ziller 2409621
+- Giovanni Castellanos 1069405
+
+
+
+![image](https://user-images.githubusercontent.com/83993066/139776278-7f5d65cd-a898-49b4-a1fb-63dfb9024c24.png)
+
+
+
+
+![image](https://user-images.githubusercontent.com/83993066/139776626-4f42c328-2872-4f81-bf37-9e07baac98f0.png)
---
+++ wfCompilador/wfCompilador/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace wfCompilador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.gramatica = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.TabPage();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.gramatica.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            this.resultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(595, 16);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.gramatica);
+            this.tabControl1.Controls.Add(this.resultado);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(786, 390);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // gramatica
+            // 
+            this.gramatica.Controls.Add(this.txtTexto);
+            this.gramatica.Controls.Add(this.groupBox1);
+            this.gramatica.Location = new System.Drawing.Point(4, 22);
+            this.gramatica.Name = "gramatica";
+            this.gramatica.Padding = new System.Windows.Forms.Padding(3);
+            this.gramatica.Size = new System.Drawing.Size(778, 364);
+            this.gramatica.TabIndex = 0;
+            this.gramatica.Text = "Compilador";
+            this.gramatica.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAnalizar);
+            this.groupBox1.Controls.Add(this.txtDocumento);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.btnAbrir);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(766, 55);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Documento";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(685, 16);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalizar.TabIndex = 3;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Enabled = false;
+            this.txtDocumento.Location = new System.Drawing.Point(7, 19);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(479, 20);
+            this.txtDocumento.TabIndex = 1;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(504, 16);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtTexto.Font = new System.Drawing.Font("Lucida Console", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTexto.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtTexto.Location = new System.Drawing.Point(29, 67);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTexto.Size = new System.Drawing.Size(714, 276);
+            this.txtTexto.TabIndex = 1;
+            // 
+            // resultado
+            // 
+            this.resultado.Controls.Add(this.btnRegresar);
+            this.resultado.Controls.Add(this.txtResultado);
+            this.resultado.Location = new System.Drawing.Point(4, 22);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(778, 364);
+            this.resultado.TabIndex = 1;
+            this.resultado.Text = "Resultado";
+            this.resultado.UseVisualStyleBackColor = true;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.Desktop;
+            this.txtResultado.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtResultado.Location = new System.Drawing.Point(3, 30);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(772, 331);
+            this.txtResultado.TabIndex = 0;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(689, 3);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 1;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Compilador";
+            this.tabControl1.ResumeLayout(false);
+            this.gramatica.ResumeLayout(false);
+            this.gramatica.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            this.resultado.ResumeLayout(false);
+            this.resultado.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage gramatica;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.TabPage resultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
+
